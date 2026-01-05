9c7354ba9efe1825c756aafe4f404e6829eea6b9
Maxvin11 <asadbekahmedov16@gmail.com>
2026-01-05 22:31:48 +0500

    fixed: domain

--- Backend/riseup/settings.py
+++ Backend/riseup/settings.py
@@ -148,9 +148,9 @@ REST_FRAMEWORK = {
 # ==========================================================
 CORS_ALLOW_ALL_ORIGINS = True
 
-# CORS_ALLOWED_ORIGINS = [
-#     "https://riseuply.vercel.app",
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "https://riseuply.vercel.app",
+]
 
 # yoki xavfsiz variant (agar front 5500 portda ishlasa)
 # CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:5500"]
